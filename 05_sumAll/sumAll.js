2f4aad8404c1766a4ecd695fd5d3867cc8284c62
Vinzon <luongvinson@gmail.com>
2022-05-27 19:39:07 -0600

    complete sumAll test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+let i = Math.min(a, b); // any sequence of a & b
+let total = 0;
 
-};
+// check for positive integers only
+if (Number.isInteger(a) && Number.isInteger(b) && a * b >= 0){
+    while (i <= Math.max(a,b)){
+        total += i;
+        i++;
+        }
+        return total;
+    } else {
+        return 'ERROR';
+    }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
