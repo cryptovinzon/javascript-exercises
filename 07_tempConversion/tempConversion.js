be79121f957c3094617c70c45c9b26b5e7b3e5c5
Vinzon <luongvinson@gmail.com>
2022-05-27 20:09:03 -0600

    complete tempConversion test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,10 @@
-const ftoc = function() {
-
+// multiplies answers by 10, rounds, then moves decimal left
+const ftoc = function(x) {
+  return Math.round((x - 32) * 5/9 * 10) / 10
 };
 
-const ctof = function() {
-
+const ctof = function(y) {
+  return Math.round((y * 9/5 + 32) * 10) / 10
 };
 
 // Do not edit below this line
