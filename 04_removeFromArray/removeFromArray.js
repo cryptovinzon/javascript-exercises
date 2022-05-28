610a8b4708be86064d4b97586197d5a0a515c65b
Vinzon <luongvinson@gmail.com>
2022-05-27 18:05:49 -0600

    completed removeFromArray test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    // extract first array
+    const initialArray = args[0];
 
+    // remove preceding arguments from initialArray
+    for (let i=1; i < args.length; i++) {
+        for (j=0; j < initialArray.length; j++){
+            if (args[i] === initialArray[j]){
+                initialArray.splice(j,1);
+            }
+        }
+    }
+
+    return initialArray;
 };
 
 // Do not edit below this line
