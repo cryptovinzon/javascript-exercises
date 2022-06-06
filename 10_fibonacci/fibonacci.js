4495e8180c8ac3dd08493018c195629a18d49229
Vinzon <luongvinson@gmail.com>
2022-06-06 12:29:01 -0600

    completed fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+    if (position < 0) return 'OOPS';
 
+    let array = [1 , 1];
+    for (let i = 2; i < position; i++){
+        array[i] = array[i-1] + array[i-2];
+    }
+    return array[position - 1];
 };
 
 // Do not edit below this line
