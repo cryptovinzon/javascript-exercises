771adb442f5926d14c9fa0556281bdcc41ae9ef7
Vinzon <luongvinson@gmail.com>
2022-06-06 11:40:58 -0600

    completed palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
-};
+const palindromes = function (string) {   
+    const normal = string.toLowerCase().replace(/[^a-z]/g, ''); // ^a-z: replace anything NOT a-z
+    const reversed = normal.split('').reverse().join(''); // split into array, reverse, convert back to string
+    
+    return normal === reversed ? true : false;     
+}
 
 // Do not edit below this line
 module.exports = palindromes;
