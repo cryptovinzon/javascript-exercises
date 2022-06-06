83adc66e9866a98473ace8280bac5c346252203e
Vinzon <luongvinson@gmail.com>
2022-06-06 10:26:15 -0600

    completed calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, item) => total + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, item) => total * item, 1); // set initial value to 1
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0) return 1;
+
+  let total = 1;
+  for (let i = 1; i <= a; i++) {
+    total *= i;
+    }
+    return total;
 };
+
 
 // Do not edit below this line
 module.exports = {
