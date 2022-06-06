576d76dffb27a0819bcb108d634b94141d1f6a88
Vinzon <luongvinson@gmail.com>
2022-06-06 13:49:20 -0600

    completed findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, currentPerson) => {
+        const older = getAge(oldest.yearOfDeath, oldest.yearOfBirth);
+        const current = getAge(currentPerson.yearOfDeath, currentPerson.yearOfBirth);
+        return older > current ? oldest : currentPerson; // loop through, return oldest
+    })
 };
+
+function getAge(latest, birth){
+    if (!latest) {
+        latest = new Date().getFullYear(); // current date if alive
+    }
+    return latest - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
